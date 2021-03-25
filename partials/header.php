2ac71643893a44775ec3936dc2c3d344075c464b
nikolabirisic <nbirisic@gmail.com>
2021-03-25 17:53:12 +0100

    Adding header essentials

--- partials/header.php
+++ partials/header.php
@@ -1,4 +1,19 @@
 <body>
-	<header>
-
+	<header class="main-header">
+        <div class="wrapper">
+            <div class="site-info">
+                <div class="site-title">We are dissect</div>
+                <div class="site-description">Branding and Design Studio Extraordinaire.</div>
+            </div>
+            <div class="nav-bar">
+                <ul class="menu">
+                    <li class="menu-item"><a href="">Projects</a></li>
+                    <li class="menu-item"><a href="">Disciplines</a></li>
+                    <li class="menu-item"><a href="">Studio</a></li>
+                    <li class="menu-item"><a href="">Journal</a></li>
+                    <li class="menu-item"><a href="">Contact</a></li>
+                </ul>
+            </div>
+            <div class="logo"><img src="" alt="" class="logo-img"></div>
+        </div>
 	</header>
