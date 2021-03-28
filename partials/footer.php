0c1bdda8e095e807a655a788117a53c1aa5b96bf
nikolabirisic <nbirisic@gmail.com>
2021-03-28 19:22:25 +0200

    Footer content and styling

--- partials/footer.php
+++ partials/footer.php
@@ -1,6 +1,25 @@
         </main>
-		<footer>
-
+		<footer class="main-footer">
+            <div class="wrapper-full">
+                <div class="footer-top">
+                    <h1>Does your Brand need Creative <br/>
+                        and Strategic Injection?
+                    </h1>
+                    <div class="footer-quick-links">
+                        <a href="">Contact Us</a>
+                        <a href="">Testimonials</a>
+                    </div>
+                </div>
+                <div class="footer-bottom">
+                    <a href="mailto:letstalk@dissect.com" class="footer-email">letstalk@dissect.com</a>
+                    <ul class="social-links">
+                        <li><a href="">LinkedIn</a></li>
+                        <li><a href="">Medium</a></li>
+                        <li><a href="">Behance</a></li>
+                    </ul>
+                    <p class="copyright">© dissect. All rights reserved</p>
+                </div>
+            </div>
 		</footer>
 	</body>
 </html>
