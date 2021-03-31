e07299ae8315bb549d482c0d121b834d3b76d918
nikolabirisic <nbirisic@gmail.com>
2021-03-31 16:39:00 +0200

    Projects templates and general styles added

---
+++ projects.php
@@ -0,0 +1,9 @@
+<?php
+define('TITLE', 'Dissect | Projects');
+define('HERO_TITLE', 'Brands we created.');
+require 'partials/head.php';
+require 'partials/header.php';
+require 'partials/hero.php';
+require 'templates/projects-template.php';
+require 'partials/footer.php';
+
---
+++ templates/projects-template.php
@@ -0,0 +1,99 @@
+<section class="projects-showcase">
+    <div class="wrapper-full">
+        <div class="holder">
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="project-philosophy">
+    <div class="wrapper-full">
+        <h2>“Internal stakeholders are extremely impressed with dops.digital’s work. Customers can expect a highly capable, well-organized, and overall excellent team.”</h2>
+        <h5>Marko Andersen, Appy CEO</h5>
+    </div>
+</section>
+<section class="projects-showcase">
+    <div class="wrapper-full">
+        <div class="holder">
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+            <div class="tb-6">
+                <div class="project-holder">
+                    <img src="./assets/images/video-reel-placeholder.jpg" alt="project">
+                    <div class="project-info">
+                        <h5>Cimet Software</h5>
+                        <p>Software Development Agency</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
