b9bc085421b4a28e64fd111c2711de9531566681
nikolabirisic <nbirisic@gmail.com>
2021-04-01 13:07:09 +0200

    Adding Contact templates and general styles

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+define('TITLE', 'Dissect | Contact');
+define('HERO_TITLE', 'We are here to help. <br> Let\'s talk!');
+require 'partials/head.php';
+require 'partials/header.php';
+require 'partials/hero.php';
+require 'templates/contact-template.php';
+require 'partials/footer.php';
+
---
+++ templates/contact-template.php
@@ -0,0 +1,35 @@
+<section class="contact-address">
+    <div class="wrapper-full">
+        <div class="holder">
+            <div class="tb-2">
+                <h5>Contact</h5>
+            </div>
+            <div class="tb-2">
+                <a href="mailto:letstalk@dissect.com">letstalk@dissect.com</a>
+                <a href="tel:+381 63 877 3174">+381 63 877 3174</a>
+            </div>
+            <div class="tb-2">
+                <p>Milana Toplice 16 <br> 21000 Novi Sad</p>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="contact-directors">
+    <div class="wrapper-full">
+        <div class="holder">
+            <div class="tb-2">
+                <h5>Creative Directors</h5>
+            </div>
+            <div class="tb-2">
+                <p>Marko Smitran</p>
+                <a href="mailto:m@dissect.com">m@dissect.com</a>
+                <a href="tel:+381 63 877 3174">+381 63 877 3174</a>
+            </div>
+            <div class="tb-2">
+                <p>Tijana Samardzic</p>
+                <a href="mailto:m@dissect.com">t@dissect.com</a>
+                <a href="tel:+381 63 877 3174">+381 63 877 3174</a>
+            </div>
+        </div>
+    </div>
+</section>
