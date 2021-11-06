e255df56ee6dde51f672ea4c2873da93f7c8ecb5
nikolabirisic <nbirisic@gmail.com>
2021-11-06 14:36:51 +0100

    Adding images to the home

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,106 +1,97 @@
 <section class="home-reel">
 	<div class="wrapper-full">
-		<div class="holder video-holder">
-			<h4 class="video-cta">Play Video</h4>
-			<img src="./assets/images/video-reel-placeholder.jpg" alt="" class="tb-12 video-reel">
-		</div>
-			<div class="reel-info holder">
-				<h3 class="tb-6 reel-title">The practice</h3>
-				<div class="tb-6 reel-desc">
-					<h3>Dissect   specialises in brand identity systems, visual communication, web, print & digital experiences, and brand development. We help brands discover and define the story, visualize and verbalize it purposefuly, spread the message, and inspire.</h3>
-					<a href="" class="read-more">Know more</a>
-				</div>
-			</div>
-		</div>
+<!--		<div class="holder video-holder">-->
+<!--			<h4 class="video-cta">Play Video</h4>-->
+<!--			<img src="./assets/images/01.jpg" alt="" class="tb-12 video-reel">-->
+<!--		</div>-->
+<!--			<div class="reel-info holder">-->
+<!--				<h3 class="tb-6 reel-title">The practice</h3>-->
+<!--				<div class="tb-6 reel-desc">-->
+<!--					<h3>Dissect   specialises in brand identity systems, visual communication, web, print & digital experiences, and brand development. We help brands discover and define the story, visualize and verbalize it purposefuly, spread the message, and inspire.</h3>-->
+<!--					<a href="" class="read-more">Know more</a>-->
+<!--				</div>-->
+<!--			</div>-->
+<!--		</div>-->
+        <div class="holder">
+            <div class="tb-7 work-example">
+                <img src="./assets/images/01-home-gif.gif" alt="work-example">
+                <div class="work-info">
+                    <h3>Cimet Software</h3>
+                    <h4>Software Development Agency</h4>
+                </div>
+            </div>
+        </div>
+        <div class="holder">
+            <div class="tb-7 tb-push-5 work-example work-example-right">
+                <img src="./assets/images/02-home.jpg" alt="work-example">
+                <div class="work-info">
+                    <h3>nude&rude</h3>
+                    <h4>Cosmetic Brand with Attitude</h4>
+                </div>
+            </div>
+        </div>
 	</div>
-</section>
+<!--</section>-->
 <section class="home-work">
 	<div class="wrapper-full">
 		<div class="holder">
-			<div  class="ds-9 txt-left work-title">
-				<h2>A small and dedicated team, creating exceptional work for exceptional businesses.</h2>
-			</div>
+            <div  class="ds-4 txt-left work-title">
+                <h3>Disciplines.</h3>
+            </div>
+            <div  class="ds-8 txt-left work-title">
+                <h3>Dissect™ specializes in brand strategy and brand identity systems. We help companies define and narrate a compelling story about their business through visual and verbal communication that seamlessly spans the digital and physical worlds.</h3>
+                <a href="">Know more</a>
+            </div>
 		</div>
 		<div class="holder">
-			<div class="tb-7 work-example">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
+			<div class="tb-4 work-example">
+				<img src="./assets/images/03-home.jpg" alt="work-example">
 				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
+					<h3>Vai</h3>
+					<h4>Seamless Business Communication</h4>
+				</div>
+			</div>
+            <div class="tb-5 tb-push-3 work-example">
+                <img src="./assets/images/04-home.jpg" alt="work-example">
+                <div class="work-info">
+                    <h3>Architecture Pour Tous</h3>
+                    <h4>Architecture Firme Montréal</h4>
+                </div>
+            </div>
+		</div>
+		<div class="holder">
+			<div class="tb-7 tb-push-5 work-example work-example-full">
+                <video width="890" height="600" loop muted autoplay>
+                    <source src="./assets/videos/05-home-mp4.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+				<div class="work-info">
+					<h3>The Unicorn Museum</h3>
+					<h4>The Land of Everywhere</h4>
 				</div>
 			</div>
 		</div>
 		<div class="holder">
-			<div class="tb-12 work-example work-example-full">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
+            <div  class="ds-7 txt-left work-title">
+                <h2>A skillful and dedicated team, creating exceptional work for exceptional businesses.</h2>
+                <a href="">Our studio</a>
+            </div>
 		</div>
 		<div class="holder">
-			<div class="tb-7 tb-push-5 work-example work-example-right">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
-		</div>
-		<div class="holder">
-			<div class="tb-7 work-example">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
-		</div>
-		<div class="holder">
-			<div  class="ds-9 txt-left work-title">
-				<h2>We know how your Brand breathe, how it comunicate, what organs it use to see and how it look.</h2>
-				<ul class="work-links">
-					<li><a href="">Our Devices</a></li>
-					<li><a href="">Contact Us</a></li>
-				</ul>
-			</div>
-		</div>
-		<div class="holder">
-			<div class="tb-7 tb-push-5 work-example work-example-right">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
-		</div>
-		<div class="holder">
-			<div class="tb-12 work-example work-example-full">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
-		</div>
-		<div class="holder">
-			<div class="tb-5 tb-push-7 work-example work-example-right">
-				<img src="./assets/images/portrait-img-work.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
-		</div>
-		<div class="holder">
-			<div class="tb-7 work-example">
-				<img src="./assets/images/work-example-home.jpg" alt="work-example">
-				<div class="work-info">
-					<h3>Cimet Software</h3>
-					<h4>Software Development Agency</h4>
-				</div>
-			</div>
+            <div class="tb-4 work-example work-example-right">
+                <img src="./assets/images/06-home.jpg" alt="work-example">
+                <div class="work-info">
+                    <h3>Ula loops</h3>
+                    <h4>The Habit of Movement</h4>
+                </div>
+            </div>
+            <div class="tb-5 tb-push-3 work-example">
+                <img src="./assets/images/07-home.jpg" alt="work-example">
+                <div class="work-info">
+                    <h3>Drad</h3>
+                    <h4>Fresh look for Stuttgart’s renowned Architect</h4>
+                </div>
+            </div>
 		</div>
 	</div>
 </section>
