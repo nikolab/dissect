49cec691a5f9dfc563e73d670cf1e49e395f81f0
nikolabirisic <nbirisic@gmail.com>
2024-04-24 18:00:34 +0800

    Adding new folders

--- templates/homepage.php
+++ templates/homepage.php
@@ -14,8 +14,10 @@
 <!--		</div>-->
         <div class="holder">
             <div class="tb-7 work-example">
-                <img src="./assets/images/01-home-gif.gif" alt="work-example">
-                <div class="work-info">
+                <video width="100%" height="auto" loop muted autoplay>
+                    <source src="./assets/videos/cimet_software-tech_branding_project-1080x718.mp4" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>                <div class="work-info">
                     <h3>Cimet Software</h3>
                     <h4>Software Development Agency</h4>
                 </div>
@@ -23,7 +25,9 @@
         </div>
         <div class="holder">
             <div class="tb-7 tb-push-5 work-example work-example-right">
-                <img src="./assets/images/02-home.jpg" alt="work-example">
+                <img src="./assets/images/nude_and_rude-beauty_branding_project-1200x800-min.jpg"
+                     srcset="./assets/images/nude_and_rude-beauty_branding_project-800x533-min.jpg 800w, ./assets/images/nude_and_rude-beauty_branding_project-600x400-min.jpg 600w"
+                     alt="nude&rude">
                 <div class="work-info">
                     <h3>nude&rude</h3>
                     <h4>Cosmetic Brand with Attitude</h4>
@@ -45,14 +49,18 @@
 		</div>
 		<div class="holder">
 			<div class="tb-4 work-example">
-				<img src="./assets/images/03-home.jpg" alt="work-example">
-				<div class="work-info">
+                <img src="./assets/images/vai-telecommunications_branding_project-1600x2124-min.jpg"
+                     srcset="./assets/images/vai-telecommunications_branding_project-800x1062-min.jpg 800w, ./assets/images/vai-telecommunications_branding_project-600x796-min.jpg 600w"
+                     alt="Vai">
+                <div class="work-info">
 					<h3>Vai</h3>
 					<h4>Seamless Business Communication</h4>
 				</div>
 			</div>
             <div class="tb-5 tb-push-3 work-example">
-                <img src="./assets/images/04-home.jpg" alt="work-example">
+                <img src="./assets/images/Ap2_architecture_studio-arhitect_branding_project-1200x800-min.jpg"
+                     srcset="./assets/images/Ap2_architecture_studio-arhitect_branding_project-800x533-min.jpg 800w, ./assets/images/Ap2_architecture_studio-arhitect_branding_project-600x400-min.jpg 600w"
+                     alt="Architecture Pour Tous">
                 <div class="work-info">
                     <h3>Architecture Pour Tous</h3>
                     <h4>Architecture Firme Montréal</h4>
@@ -61,8 +69,8 @@
 		</div>
 		<div class="holder">
 			<div class="tb-7 tb-push-5 work-example work-example-full">
-                <video width="890" height="600" loop muted autoplay>
-                    <source src="./assets/videos/05-home-mp4.mp4" type="video/mp4">
+                <video width="100%" height="auto" loop muted autoplay>
+                    <source src="./assets/videos/The_unicorn_museum-museum_branding_project-1621x1080.mp4" type="video/mp4">
                     Your browser does not support the video tag.
                 </video>
 				<div class="work-info">
@@ -79,14 +87,18 @@
 		</div>
 		<div class="holder">
             <div class="tb-4 work-example work-example-right">
-                <img src="./assets/images/06-home.jpg" alt="work-example">
+                <img src="./assets/images/Ula-fitness_branding_project-1600x2123-min.jpg"
+                     srcset="./assets/images/Ula-fitness_branding_project-800x1062-min.jpg 800w, ./assets/images/Ula-fitness_branding_project-600x796-min.jpg 600w"
+                     alt="Ula loops">
                 <div class="work-info">
                     <h3>Ula loops</h3>
                     <h4>The Habit of Movement</h4>
                 </div>
             </div>
             <div class="tb-5 tb-push-3 work-example">
-                <img src="./assets/images/07-home.jpg" alt="work-example">
+            <img src="./assets/images/daniela_resch-architecture_branding_project-600x400-min.jpg"
+                 srcset="./assets/images/daniela_resch-architecture_branding_project-800x533-min.jpg 800w, ./assets/images/daniela_resch-architecture_branding_project-600x400-min.jpg 600w"
+                     alt="Drad">
                 <div class="work-info">
                     <h3>Drad</h3>
                     <h4>Fresh look for Stuttgart’s renowned Architect</h4>
